b2d9ae75d08982af8a165a3356af42099bb24e87
Neel Pai <neelpai3702@live.com>
2019-01-11 12:02:51 -0600

    progress?

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-
+import { PythonShell } from 'python-shell';
+var fanOff = 'fanOff.py';
+var fanOn = 'fanOn.py';
+var lightOff = 'lightOff.py';
+var lightOn = 'lightOn.py';
 var fan = false;
 var light = false;
 var fanB = document.getElementById("id_wf327dQCK");
 var lightB = document.getElementById("id_V1PdXwqzK");
 var stateF = 0;
 var stateL = 0;
-const spawn = require("child_process").spawn;
-fanB.addEventListener('touchstart', function touchF(ev) {
+
+fanB.addEventListener('touchstart', function touchF() {
     stateF++;
     if(stateF %2==0){
         fan=false;
-        PythonShell.run('fanOff.py', null, function (err) {
+        PythonShell.run(fanOff, null, function (err) {
             if (err) throw err;
-            console.log('finished');});
+            console.log('finished');
+        });
         console.log(fan);
     }
     else{
         fan=true;
-        PythonShell.run('fanOn.py', null, function (err) {
+        PythonShell.run(fanOn, null, function (err) {
             if (err) throw err;
             console.log('finished');
         });
@@ -29,12 +34,18 @@ lightB.addEventListener('touchstart', function touchF() {
     stateL++;
     if (stateL % 2 == 0) {
         light = false;
-        const pythonProcess = spawn('python', ["lightOff.py"]);
+        PythonShell.run(lightOff, null, function (err) {
+            if (err) throw err;
+            console.log('finished');
+        });
         console.log(light);
     }
     else {
         light = true;
-        const pythonProcess = spawn('python', ["lightOn.py"]);
+        PythonShell.run(lightOn, null, function (err) {
+            if (err) throw err;
+            console.log('finished');
+        });
         console.log(light);
     }
 
